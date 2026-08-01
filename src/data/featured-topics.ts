63e7cffbeed0fa4105525add1b211c700ccd5e05
Malcolm Bastien <malcolmbastien@gmail.com>
2026-08-01 18:58:45 -0400

    Add popular topics section to topics index page

---
+++ src/data/featured-topics.ts
@@ -0,0 +1,18 @@
+export const FEATURED_TOPICS: string[] = [
+    "Agile",
+    "AI",
+    "Leadership",
+    "Kanban",
+    "Coaching",
+    "Software",
+    "Productivity",
+];
+
+export const EXCLUDED_FROM_POPULAR: string[] = [
+    "General",
+    "Image",
+    "Images",
+    "Year-In-Review",
+];
+
+export const POPULAR_LIMIT = 12;
